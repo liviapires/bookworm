c25c89c49229a28c6eea3ccdd67dba49162f288a
liviapires <liviapso2012@gmail.com>
2024-03-14 17:09:22 -0300

    Editando Categories e Home

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -5,6 +5,6 @@ const categoryController = require("../controllers/categoryController");
 
 router.get("/category/:id", categoryController.categoryView);
 
-router.get("/categories", categoryController.categoriesView);
+// router.get("/categories", categoryController.categoriesView);
 
 module.exports = router;
