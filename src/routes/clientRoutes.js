6b6bd1e3d06b848cf8e41a38429f3963522efa8b
Livia Pires <liviapso2012@gmail.com>
2024-03-22 22:40:24 -0300

    Refatorando Routes

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const router = express.Router();
 
-const clientController = require("../controllers/clientController");
+const userController = require("../controllers/userController");
 
 // GET ROUTES
 
-router.get("/admin/clients", clientController.clientsView);
-router.get("/admin/client/:id", clientController.clientView);
-router.get("/signin", clientController.signinView);
-router.get("/clients/delete/:id", clientController.deleteClient);
-router.get("/clients/edit/:id", clientController.editClientView);
-router.get("/client/edit/phone/:id", clientController.editPhoneView);
-router.get("/client/edit/address/:id", clientController.editAddressView);
+router.get("/admin/clients", userController.clientsView);
+router.get("/admin/client/:id", userController.clientView);
+router.get("/signin", userController.signinView);
+router.get("/clients/delete/:id", userController.deleteClient);
+router.get("/clients/edit/:id", userController.editClientView);
+router.get("/client/edit/phone/:id", userController.editPhoneView);
+router.get("/client/edit/address/:id", userController.editAddressView);
 
 
 // POST ROUTES
 
-router.post("/signin/newClient", clientController.createClient);
-router.post("/editClient", clientController.updateClient);
+router.post("/signin/newClient", userController.createClient);
+router.post("/editClient", userController.updateClient);
 
 module.exports = router;
