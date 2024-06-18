218fc18bd8abb952a94372b2a848f47e9db201c1
Livia Pires <liviapso2012@gmail.com>
2024-06-17 22:54:30 -0300

    refactor: Adjust image sizes and add book links in category view

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -5,14 +5,14 @@ const aBook = new Book();
 const aCategory = new Category();
 
 async function oneBookView (req, res) {
-    // let livro = await aBook.getBookById(req.params.id);
+    let livro = await aBook.getBookById(req.params.id);
 
     let categories = await aCategory.getAllCategories();
 
     res.render('book', {
         title: 'Livro',
         categories: categories,
-        livro: livro
+        livro: livro[0]
     });
 }
 
